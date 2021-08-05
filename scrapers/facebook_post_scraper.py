44214e33e65e2519f2dda70e5dd590cb28cb3c9a
saadaoui-salah <salahsaadaoui8@gmail.com>
2021-08-05 21:05:50 +0200

    add get description and get account functions

--- scrapers/facebook_post_scraper.py
+++ scrapers/facebook_post_scraper.py
@@ -1,16 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.remote.webdriver import _make_w3c_caps
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
+from selenium.common.exceptions import TimeoutException
 import time
+from datetime import datetime
+
 class Parameters:
     URLS = ["https://www.facebook.com/carta.dz"]
-    MAX_RESULTS = 100
+    MAX_RESULTS = 20
     HIDE_BROWSER = False
-    USERNAME = "swileh.ly"
-    PASSWORD = "zehzeh0809"
 
 
 class PostScraper:
@@ -18,71 +17,121 @@ class PostScraper:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.headless = Parameters.HIDE_BROWSER
         self.driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\chromedriver.exe", options=chrome_options)
-        self.driver.get("https://www.facebook.com/")
-    
+        self.last_height = 0
+        self.toolbar_data = {
+            'like':0,
+            'love':0,
+            'care':0,
+            'haha':0,
+            'wow':0,
+            'sad':0,
+            'angry':0,
+        }
     def wait(self, xpath):
         WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.XPATH, xpath)))
-    
-    def login(self):
-        self.driver.find_element_by_css_selector("input[name='email']").send_keys(Parameters.USERNAME)
-        self.driver.find_element_by_css_selector("input[name='pass']").send_keys(Parameters.PASSWORD)
-        self.driver.find_element_by_css_selector("button[name='login']").click()
-        
-    def go_to_page(self, url):
-        self.driver.get(url)
 
-    def get_account(self,post):
-        pass
-    
+    def skip_login_tab(self):
+        self.driver.find_element_by_css_selector("a[data-nocookies='1']").click()
+
+    def go_to_page(self, url, is_post=False):
+        self.driver.get(url)
+        post = self.driver.find_element_by_css_selector('div[data-visualcompletion="ignore-dynamic"]')
+        return post 
+
+    def get_account(self):
+        account = self.driver.find_element_by_css_selector('meta[property="og:title"]').get_attribute("content")
+        return account
+
     def get_reactions(self,post):
-        pass
-    
+        reactions = post.find_elements_by_xpath('//span[@role="toolbar"]//a[@rel="dialog"]')
+        for reaction in reactions:
+            reaction_data = reaction.get_attribute('aria-label')
+            reaction_data = reaction_data.split(' ')
+            reaction_name = reaction_data[1].lower() 
+            print(reaction_data)
+            if reaction_name == "j'aime" or reaction_name == 'like':
+                self.toolbar_data['like'] = int(reaction_data[0])
+            if reaction_name == "j'adore" or reaction_name == 'love':
+                self.toolbar_data['love'] = int(reaction_data[0])
+            if reaction_name == "solidaire" or reaction_name == 'care':
+                self.toolbar_data['care'] = int(reaction_data[0])
+            if reaction_name == "haha": 
+                self.toolbar_data[reaction_name] = int(reaction_data[0])
+            if reaction_name == "wouah" or reaction_name == 'wow': 
+                self.toolbar_data['wow'] = int(reaction_data[0])
+            if reaction_name == "triste" or reaction_name == 'sad':
+                self.toolbar_data['sad'] = int(reaction_data[0])
+            if reaction_name == "grrr" or reaction_name == 'angry':
+                self.toolbar_data['angry'] = int(reaction_data[0])
+        return self.toolbar_data
+            
+
     def get_date(self,post):
-        pass
+        date = post.find_element_by_class_name('timestampContent').text
+        date = datetime.strptime(date,"%d %M %Y")
+        return date
     
-    def get_content(self,post):
-        description = post.find_element_by_css_selector("div[data-ad-preview='message']")
-        description.find_element_by_css_selector('div[role="button"]').click()
-        return description.text
+    def get_description(self,post):
+        text = '' 
+        lines = post.find_elements_by_css_selector('div[dir]>p')
+        for line in lines:
+            text += line.text
+        return text
 
     def get_posts(self, urls, max_results):
         posts = []
-        while len(posts) < max_results:
-            for url in urls:
-                print(url)
-                self.go_to_page(url)
-                new_posts = self.driver.find_elements_by_css_selector('div[role="feed"]')
+        for url in urls:
+            print(f"Go to {url}...")
+            self.go_to_page(url)
+            while len(posts) < max_results:
+                new_posts = self.driver.find_elements_by_css_selector('div[data-visualcompletion="ignore-dynamic"]')
                 for post in new_posts:
+                    if len(posts) >= max_results:
+                        print(f"\t{len(posts)} Posts Scrapped")        
+                        return posts
                     if post in posts:
-                        continue
-                    posts += post
-                return posts
-            self.scroll()
-        print(len(posts))
+                        pass
+                    else : 
+                        posts.append(post)
+                end  = self.scroll()
+                if end:
+                    break
+        print(f"\t{len(posts)} Posts Scrapped")
         return posts
 
     def scroll(self):
         SCROLL_PAUSE_TIME = 2
-        last_height = 0
-        while True:
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(SCROLL_PAUSE_TIME)
-            new_height = self.driver.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                return
-            if Parameters.MAX_SCROLL == 0:
-                return
-            Parameters.MAX_SCROLL -= 1
-            last_height = new_height
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(SCROLL_PAUSE_TIME)
+        new_height = self.driver.execute_script("return document.body.scrollHeight")
+        if new_height == self.last_height:
+            return True
+        self.last_height = new_height
+        return False
 
 def start_scraper(urls, max_results):
     scraper = PostScraper()
-    scraper.login()
     posts = scraper.get_posts(urls, max_results)
+    scraper.skip_login_tab()
+    account = scraper.get_account()
+    print(f"\tAccount: '{account}'")
     for post in posts:
+        print(post)
         description = scraper.get_description(post)
-        print(description)
-
+        print(f"\tDescription: '{description}'")
+        #date = scraper.get_date(post)
+        #print(f"\tDate: '{date}'")
+        scraper.get_reactions(post)
+        print(f"\treactions: '{scraper.toolbar_data}'")
+        scraper.toolbar_data = {
+            'like':0,
+            'love':0,
+            'care':0,
+            'haha':0,
+            'wow':0,
+            'sad':0,
+            'angry':0,
+        } 
 if __name__ == '__main__':
     start_scraper(
         urls= Parameters.URLS,
